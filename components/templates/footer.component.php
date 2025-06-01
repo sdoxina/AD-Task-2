a89e4309f096a3ca8bad92a26f7e4a5b0abfb402
sdoxina <202311026@fit.edu.ph>
2025-06-01 22:47:31 +0800

    feat(footer): add footer component with structured layout and copyright notice

---
+++ components/templates/footer.component.php
@@ -0,0 +1,28 @@
+<?php
+// components/templates/footer.component.php
+?>
+
+<footer class="footer">
+  <div class="container">
+    <footer class="py-5">
+      <div class="row">
+        <div class="col-6 col-md-2 mb-3">
+          <ul class="footer flex-column list-unstyled">
+            <li class="footer-item mb-2"><a href="index.php?home_clicked=1" class="footer-link p-0 text-body-secondary">Home</a></li>
+            <li class="footer-item mb-2"><a href="./pages/items/index.php" class="footer-link p-0 text-body-secondary">Items</a></li>
+          </ul>
+        </div>
+        <div class="col-6 col-md-2 mb-3">
+          <h5>Web Designer</h5>
+          <h6>Shane D. Oxina</h6>
+        </div>
+        <div class="col-md-7 offset-md-1 mb-3">
+          <img src="./assets/img/buttonconeLogo.png" alt="Buttoncone Logo">
+        </div>
+      </div>
+      <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
+        <p>Copyright &copy; 2025 buttoncone. All rights reserved.</p>
+      </div>
+    </footer>
+  </div>
+</footer>
