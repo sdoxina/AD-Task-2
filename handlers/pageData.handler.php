d8ba7770dde7481df14fd0d6487914c3410fd2f6
sdoxina <202311026@fit.edu.ph>
2025-06-01 22:51:26 +0800

    feat(footer): add footer links and company information to page data handler

--- handlers/pageData.handler.php
+++ handlers/pageData.handler.php
@@ -5,4 +5,18 @@ $navList = [
     ["label" => "Home", "url" => "/index.php"],
     ["label" => "Items", "url" => "/pages/items/index.php"]
 ];
+
+// For Footer
+$footerLinks = [
+    ["label" => "Home", "url" => "/index.php?home_clicked=1"],
+    ["label" => "Items", "url" => "/pages/items/index.php"]
+];
+
+$webDesigner = [
+    "name" => "Shane D. Oxina",
+    "role" => "Web Designer"
+];
+
+$companyLogo = "/assets/img/buttonconeLogo.png";
+$companyCopyright = "© 2025 buttoncone. All rights reserved.";
 ?>
