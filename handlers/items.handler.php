de7191ae2ccba087288d1b3119c9f31da2ef4c8c
sdoxina <202311026@fit.edu.ph>
2025-06-02 06:23:54 +0800

    feat(items): implement getAllItems function to retrieve product details

---
+++ handlers/items.handler.php
@@ -0,0 +1,12 @@
+<?php
+    function getAllItems(): array {
+    return [
+        ['name' => 'Crochet Cardigan', 'category' => 'clothing', 'img' => './assets/img/crochet-cardigan.jpg'],
+        ['name' => 'Bunny Plushie', 'category' => 'plushie', 'img' => './assets/img/crochet-bunny.jpg'],
+        ['name' => 'Tulip Bag', 'category' => 'bags', 'img' => './assets/img/crochet-tulip-bag.jpg'],
+        ['name' => 'Crochet Hat', 'category' => 'clothing', 'img' => './assets/img/crochet-hat.jpg'],
+        ['name' => 'Penguin Plushie', 'category' => 'plushie', 'img' => './assets/img/crochet-penguin.jpg'],
+        ['name' => 'Water Lily Bag', 'category' => 'bags', 'img' => './assets/img/crochet-lily.jpg'],
+    ];
+}
+?>
