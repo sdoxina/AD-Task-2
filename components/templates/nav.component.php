0e5e4504572524ab8f20ac71ab68472a9cfff87d
sdoxina <202311026@fit.edu.ph>
2025-06-01 22:46:48 +0800

    feat(navigation): add navigation component with responsive design

---
+++ components/templates/nav.component.php
@@ -0,0 +1,32 @@
+<?php
+// components/templates/nav.component.php
+?>
+
+<nav class="navbar navbar-expand-lg fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand me-auto d-flex align-items-center" href="index.php?home_clicked=1">
+      <img src="./assets/img/buttonconeLogo.png" alt="Logo" class="navbar-img">
+    </a>
+    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title d-flex align-items-center" id="offcanvasNavbarLabel">
+          <img src="./assets/img/buttonconeLogo.png" alt="Buttoncone Logo">
+        </h5>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <ul class="navbar-nav ms-auto pe-3">
+          <li class="nav-item">
+            <a class="nav-link active mx-lg-2" aria-current="page" href="index.php?home_clicked=1">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link mx-lg-2" aria-current="page" href="./pages/items/index.php">Items</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <button class="navbar-toggler pe-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  </div>
+</nav>
