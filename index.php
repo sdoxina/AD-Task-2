a78c152424cf875c02d54d537908c043e3562dc1
sdoxina <202311026@fit.edu.ph>
2025-06-01 22:32:10 +0800

    feat(landing): add favicon

--- index.php
+++ index.php
@@ -20,6 +20,7 @@ $images = getShowcaseImages();
     <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="icon" type="image/x-icon" href="./assets/img/buttonconeFav.png">
 </head>
 <body>
     <!-- Navigation Bar -->
