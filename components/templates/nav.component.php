3c164375d8b71587e8dd9ae6457ff7f1c453a3f0
sdoxina <202311026@fit.edu.ph>
2025-06-01 22:49:45 +0800

    refactor(navigation): update navigation links to use dynamic data from navList

--- components/templates/nav.component.php
+++ components/templates/nav.component.php
@@ -1,27 +1,28 @@
 <?php
-// components/templates/nav.component.php
+require_once BASE_PATH . '/handlers/pageData.handler.php';
 ?>
 
 <nav class="navbar navbar-expand-lg fixed-top">
   <div class="container-fluid">
-    <a class="navbar-brand me-auto d-flex align-items-center" href="index.php?home_clicked=1">
-      <img src="./assets/img/buttonconeLogo.png" alt="Logo" class="navbar-img">
+    <a class="navbar-brand me-auto d-flex align-items-center" href="/index.php?home_clicked=1">
+      <img src="/assets/img/buttonconeLogo.png" alt="Logo" class="navbar-img">
     </a>
     <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
       <div class="offcanvas-header">
         <h5 class="offcanvas-title d-flex align-items-center" id="offcanvasNavbarLabel">
-          <img src="./assets/img/buttonconeLogo.png" alt="Buttoncone Logo">
+          <img src="/assets/img/buttonconeLogo.png" alt="Buttoncone Logo">
         </h5>
         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
       </div>
       <div class="offcanvas-body">
         <ul class="navbar-nav ms-auto pe-3">
-          <li class="nav-item">
-            <a class="nav-link active mx-lg-2" aria-current="page" href="index.php?home_clicked=1">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link mx-lg-2" aria-current="page" href="./pages/items/index.php">Items</a>
-          </li>
+          <?php foreach ($navList as $navItem): ?>
+            <li class="nav-item">
+              <a class="nav-link mx-lg-2 <?= $_SERVER['REQUEST_URI'] === $navItem['url'] ? 'active' : '' ?>" href="<?= $navItem['url'] ?>">
+                <?= htmlspecialchars($navItem['label']) ?>
+              </a>
+            </li>
+          <?php endforeach; ?>
         </ul>
       </div>
     </div>
