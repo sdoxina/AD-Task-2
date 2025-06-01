8af0d17a7056e2d4abd6df827be6bf0997c7c626
sdoxina <202311026@fit.edu.ph>
2025-06-02 06:38:04 +0800

    refactor(footer): replace inline footer with component template

--- pages/items/index.php
+++ pages/items/index.php
@@ -37,21 +37,7 @@ $categories = ['clothing', 'plushie', 'bags'];
 
 
     <!-- Footer -->
-    <footer class="footer">
-        <div class="container">
-            <div class="footer-links">
-                <ul>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Services</a></li>
-                    <li><a href="#">Resources</a></li>
-                </ul>
-            </div>
-            <div class="footer-logo">
-                <img src="" alt="">
-            </div>
-            <p>&copy; 2025 butttoncone. All rights reserved.</p>
-        </div>
-    </footer>
+    <?php require_once BASE_PATH . '/components/templates/footer.component.php'; ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="./assets/js/script.js"></script>
